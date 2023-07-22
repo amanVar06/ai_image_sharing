a1d2462820bc95f81fc9ff5ec69f660b1cd8c0b4
amanVar06 <amanvarshney212@gmail.com>
2023-07-22 13:33:54 +0530

    ....

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/posts", postRoutes);
